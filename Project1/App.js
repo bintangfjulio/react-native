d1d39d839b8107cec41236b669b721389c340325
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2024-09-16 19:34:14 +0700

    init

---
+++ Project1/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import FirstScreen from './src/screens/FirstScreen';
+
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      <FirstScreen />
+    </SafeAreaProvider>
+  );
+};
+
+export default App;
