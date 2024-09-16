b5bb37c8c76b4bb00dfe60ae89db8718fe9c4e04
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2024-09-16 20:07:21 +0700

    M1

--- Project1/App.js
+++ Project1/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import FirstScreen from './src/screens/FirstScreen';
+// import FirstScreen from './src/screens/FirstScreen';
+import ChalengeScreen from './src/screens/FirstScreen';
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <FirstScreen />
+      {/* <FirstScreen /> */}
+      <ChalengeScreen />
     </SafeAreaProvider>
   );
 };
--- Project1/src/screens/FirstScreen.js
+++ Project1/src/screens/FirstScreen.js
@@ -1,12 +1,48 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 
 const FirstScreen = () => {
+  const openAlert = () => {
+    alert('You clicked the button!');
+  };
+
   return (
-    <View>
-      <Text>Hello World!</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{margin: 16}}>
+        Hello<Text style={{color: 'red'}}> World</Text>!
+      </Text>
+      <Button title="This is button" onPress={openAlert} />
     </View>
   );
 };
 
-export default FirstScreen;
+const ChalengeScreen = () => {
+  const showAlert = () => {
+    alert('You are awesome!');
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 12,
+      }}>
+      <Text style={{color: 'green', margin: 12, fontSize: 16}}>
+        Let's Learn React Native Framework
+      </Text>
+      <Text style={{margin: 8}}>My name is Alya</Text>
+      <Text style={{margin: 8}}>
+        Now I'm learning about{' '}
+        <Text style={{color: 'blue'}}>React Native Components</Text> to build
+        the user interface for android apps
+      </Text>
+      <Text style={{margin: 8, color: 'red'}}>I Love Coding</Text>
+      <Button title="Click Me" onPress={showAlert} />
+    </View>
+  );
+};
+
+// export default FirstScreen;
+export default ChalengeScreen;
